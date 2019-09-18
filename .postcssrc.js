df797fb90199e23db71a476eb23c2e31d895c91f
Ozmar Mendoza <ozmarmen@gmail.com>
2019-09-18 13:29:54 -0500

    Added font-magician

--- .postcssrc.js
+++ .postcssrc.js
@@ -1,5 +1,10 @@
 module.exports = {
 	plugins: [
+		require('postcss-font-magician')({
+			'display': 'fallback',
+			'foundries': 'google',
+			'protocol': 'https:'
+		}),
 		require('autoprefixer'),
 	],
 };
