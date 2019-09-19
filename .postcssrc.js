f91d0eda2d8264e01426085710deccce8a59cb90
Ozmar Mendoza <ozmarmen@gmail.com>
2019-09-19 14:51:54 -0500

    Added hosted to foundries

--- .postcssrc.js
+++ .postcssrc.js
@@ -2,7 +2,7 @@ module.exports = {
 	plugins: [
 		require('postcss-font-magician')({
 			'display': 'fallback',
-			'foundries': 'google',
+			'foundries': 'google hosted',
 			'protocol': 'https:'
 		}),
 		require('autoprefixer'),
